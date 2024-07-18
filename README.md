559156fb167c7614d33db88d2759cf39231e857b
Alex Araujo <alexsaspbr@gmail.com>
2024-07-18 08:50:10 -0300

    adicionando material

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
-# Exercicio
+# Exercicios
 
-1. Crie uma classe que represente o cadastro de uma pessoa fisica com os seguintes campos: 
-
-- CPF
-- Nome
-- Idade
-- Estado Civil
-
-Cada campo deve usar o tipo que melhor se encaixe, de acordo com o que foi exposto em aula.
-Para padronizar a Classe se chamará PessoaFisica e ja esta criada no projeto.
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        /*Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite seu nome");
+        String nome = entrada.nextLine();
+        System.out.println("Digite sua idade");
+        Integer idade = Integer.parseInt(entrada.nextLine());
+        System.out.printf("Hello, %s sua idade é %d", nome, idade);*/
 
 
+        new MaiorNumero().maiorNumero();
 
     }
 
